7cd4597945ab05e4c7476cfff7e20e03bc7a7364
Oliver Wasner <oli.wasner@hotmail.com>
2023-10-15 18:24:53 +0200

    first commit

---
+++ spa-aspnet-react-game/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace spa_aspnet_react_game.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? Nickname { get; set; }
+
+        //Navigation Property
+        public IEnumerable<GameModel>? Games { get; set; }
+    }
+}
---
+++ spa-aspnet-react-game/Models/GameModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace spa_aspnet_react_game.Models
+{
+    public class GameModel
+    {
+        //Primary Key
+        [Key]
+        public int GameId { get; set; }
+        //Entitys
+        public string? word { get; set; }
+        public int Attempt { get; set; } = 0;
+        public bool FoundWord { get; set; } = false;
+
+        //Foreign Key
+        [NotNull]
+        [ForeignKey("ApplicationUser")]
+        public string? UserId { get; set; }
+
+        //Navigation Property
+        public ApplicationUser? AspNetUsers { get; set; }
+
+    }
+}
